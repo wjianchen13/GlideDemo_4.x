7dab8f1de7cdac8d38649a8f11683995bca8e704
cwj <282330332@qq.com>
2025-03-25 10:23:26 +0800

    测试引入webp插件和svga插件的使用情况

--- app/src/main/java/com/example/glidedemo_4x/webp/WebpActivity.java
+++ app/src/main/java/com/example/glidedemo_4x/webp/WebpActivity.java
@@ -7,16 +7,18 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
 import com.example.glidedemo_4x.R;
+import com.opensource.svgaplayer.SVGAImageView;
 
 /**
- * 测试加载Webp的情况
+ * 测试一定缓存条件下，加载完webp之后大量加载svga，看下webp是否会首先从缓存清除掉
+ *
  */
 public class WebpActivity extends AppCompatActivity {
 
-    private String mUrl = "https://img.ayomet.com/upload/app_matter/2025-02-27/8d1652939b3f8cbcb78c17a590c3399b.webp?imageslim";
+    private String mWebpUrl = "https://img.ayomet.com/upload/app_matter/2025-02-27/8d1652939b3f8cbcb78c17a590c3399b.webp?imageslim";
+    private ImageView imgvTest;
+    private SVGAImageView imgvTest1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +27,18 @@ public class WebpActivity extends AppCompatActivity {
 
     }
 
+    private void loadSVGAFromNetwork(SVGAImageView v, String url) {
+        Glide.with(this)
+                .load(url)
+                .into(v);
+    }
+
+    public void onTest(View v) {
+        imgvTest = findViewById(R.id.image_view);
+        CoreUtils.playWebpAnim(this, imgvTest, mWebpUrl, 0, 1, null);
+    }
+
     public void onTest1(View v) {
-        ImageView imageView = findViewById(R.id.image_view);
-        CoreUtils.playWebpAnim(this, imageView, mUrl, 0, 1, null);
+
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,25 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        maven("https://developer.huawei.com/repo/")
+
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
         mavenCentral()
+        google()
+
+        maven("http://maven.aliyun.com/nexus/content/groups/public/"){
+            isAllowInsecureProtocol=true
+        }
+
+        maven ("https://maven.aliyun.com/repository/google/")
+        maven ("https://maven.aliyun.com/repository/public/")
+        maven ("https://maven.aliyun.com/repository/gradle-plugin/")
+
+        maven ("https://jitpack.io")
+        maven ("https://mirrors.huaweicloud.com/repository/maven/")
+        maven ("http://maven.faceunity.com/repository/maven-public/") {
+            isAllowInsecureProtocol=true}
+        maven ("https://s01.oss.sonatype.org/content/groups/public")
     }
 }
 
